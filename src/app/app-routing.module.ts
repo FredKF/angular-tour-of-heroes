b996a57ccee93644bb2983fbdb2724f2dea4b19e
FredKF <fedetkd86@hotmail.com>
2020-12-09 11:20:30 +0100

    App Routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Routes tell the Router which view to display
+//when a user clicks a link or pastes a URL into the browser address bar.
+
+// path: a string that matches the URL in the browser address bar.
+// component: the component that the router should create when navigating to this route.
+const routes: Routes = [
+  {path: 'heores', component: HeroesComponent}
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    // The method is called forRoot() because you configure the router 
+    // at the application's root level. The forRoot() 
+    // method supplies the service providers and directives needed for routing, 
+    // and performs the initial navigation based on the current browser URL.
+    RouterModule.forRoot(routes)    
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
