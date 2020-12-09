0b350e7bf2bb7ae7a363d3b3ef66fb540b78fc08
FredKF <fedetkd86@hotmail.com>
2020-12-09 11:52:03 +0100

    Routing Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,13 @@
 import { NgModule } from '@angular/core';
-import { HeroesComponent } from './heroes/heroes.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-//Routes tell the Router which view to display
-//when a user clicks a link or pastes a URL into the browser address bar.
-
-// path: a string that matches the URL in the browser address bar.
-// component: the component that the router should create when navigating to this route.
 const routes: Routes = [
-  {path: 'heores', component: HeroesComponent}
+  { path: 'heroes', component: HeroesComponent }
 ];
 
-
 @NgModule({
-  imports: [
-    // The method is called forRoot() because you configure the router 
-    // at the application's root level. The forRoot() 
-    // method supplies the service providers and directives needed for routing, 
-    // and performs the initial navigation based on the current browser URL.
-    RouterModule.forRoot(routes)    
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,9 +16,9 @@ import { MessagesComponent } from './messages/messages.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
-  ],
-  providers: [],
+    FormsModule,
+    AppRoutingModule
+  ],  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
