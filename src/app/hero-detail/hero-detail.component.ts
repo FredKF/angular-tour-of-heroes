1512ecafbec8a77b948a66c19b6a3b4f85c428ff
FredKF <fedetkd86@hotmail.com>
2020-12-10 18:10:20 +0100

    hero-details-navigation

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  hero: Hero;
 
   constructor(
     // The ActivatedRoute holds information about the route to this instance of the HeroDetailComponent
@@ -18,7 +19,20 @@ export class HeroDetailComponent implements OnInit {
     private location: Location
   ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void{
+    // The route.snapshot is a static image of the route information shortly after the component was created.
+    // The paramMap is a dictionary of route parameter values extracted from the URL. The "id" key returns the id of the hero to fetch.
+    // The JavaScript (+) operator converts the string to a number, which is what a hero id should be
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+  
+  goBack(): void{
+    this.location.back();
   }
 
 }
